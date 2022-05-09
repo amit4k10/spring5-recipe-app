b691ebf74b92e9a552722f007ff709ee4c436e23
amit4k10 <amit4k10@gmail.com>
2022-05-09 22:31:50 +0530

    data initialization
    data initialization

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package guru.springframework.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.domain.Category;
+import guru.springframework.repositories.CategoryRepository;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+	@Autowired
+	CategoryRepository cr;
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+    	cr.findAll().forEach(i -> System.out.println(i.getDescription()));;
         return "index";
     }
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO category (description) VALUES ('American1111');
+INSERT INTO category (description) VALUES ('Italian111');
+INSERT INTO category (description) VALUES ('Mexican111');
+INSERT INTO category (description) VALUES ('Fast Food111');
